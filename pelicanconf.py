94cd74ec550fd05d1a44f087f9e20241bd6b25cf
lhq <lhq@python279.org>
2016-05-07 12:46:07 +0800

    none

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,19 @@ from __future__ import unicode_literals
 
 AUTHOR = u'冬冬 多多 龙宝 马宝 顺顺 小弟'
 SITENAME = u'童年时光'
-SITEURL = ''
+SITEURL = 'childhood.python27.org'
 
 PATH = 'content'
+STATIC_PATHS = ['blog','downloads']
+ARTICLE_PATHS = ['blog']
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{slug}.html'
 
 TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = u'zh'
+
+THEME = 'pelican-blueidea'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -28,4 +34,4 @@ SOCIAL = ()
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
